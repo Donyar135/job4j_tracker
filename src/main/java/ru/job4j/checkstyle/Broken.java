d8ce670f63da7010d0462985c99e0f9d00124674
Daniyar <aldanov2002@bk.ru>
2023-05-15 02:02:04 +0600

    Scan with Checkstyle

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+@SuppressWarnings("checkstyle:ConstantName")
+
+public class Broken {
+    public String surname;
+    private int sizeOfEmpty = 10;
+    public static final String NEW_VALUE = "";
+    String name;
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void echo() {
+
+    }
+
+    Broken() {
+    }
+}
