0de4f408a753eff79401e2c75c4d163d8e880ad8
Daniyar <aldanov2002@bk.ru>
2023-05-19 02:42:15 +0600

    1.7. Конструктор [#173889]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -5,20 +5,30 @@ public class Error {
     private int status;
     private String message;
 
+    public Error() {
+        active = false;
+        status = 93;
+        message = "Выполнено";
+    }
+
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
     }
 
-    public void info() {
-        System.out.println(active);
-        System.out.println(status);
-        System.out.println(message);
+    public void printInfo() {
+        System.out.println("" + active);
+        System.out.println("" + status);
+        System.out.println("" + message);
     }
 
     public static void main(String[] args) {
-        Error error = new Error(true, 12, "hello");
-        error.info();
+        Error error = new Error(true, 45, "Одобрено");
+        error.printInfo();
+        Error err = new Error(false, 12, "Отклонено");
+        err.printInfo();
+        Error error1 = new Error();
+        error1.printInfo();
     }
 }
