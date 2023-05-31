efaaf535ff5aa59c91a19f90090cfdfefca61c56
Daniyar <aldanov2002@bk.ru>
2023-05-31 13:42:22 +0600

    1.8. Класс использующий сам себя [#167388 # [#167388 #419853]]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -12,11 +12,8 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        if (this.load + another.load < 100) {
-            another.load = this.load + another.load;
-        } else {
-            this.load = 0;
-        }
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 }
 
