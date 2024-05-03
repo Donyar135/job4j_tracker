05add870378cd03e9cee031a376f531662ed87c2
Daniyar <aldanov2002@bk.ru>
2024-05-03 20:11:31 +0500

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,14 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     public Item() {
-
     }
 
     public Item(String name) {
@@ -32,4 +35,13 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public DateTimeFormatter getFormatter()  {
+        return formatter;
+    }
+
 }
