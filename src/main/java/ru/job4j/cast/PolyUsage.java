587947f1d121a97cb64e2c5ee4f4fa9ffb729153
Daniyar <aldanov2002@bk.ru>
2024-07-13 21:00:37 +0500

    5.1. Зачем нужно приведение типов [#310019]

--- src/main/java/ru/job4j/cast/PolyUsage.java
+++ src/main/java/ru/job4j/cast/PolyUsage.java
@@ -2,14 +2,13 @@ package ru.job4j.cast;
 
 public class PolyUsage {
     public static void main(String[] args) {
-        Animal cow = new Cow();
-        Animal goose = new Goose();
-        Animal dog = new Dog();
-        Animal guineaPig = new GuineaPig();
+        Vehicle bus = new Bus();
+        Vehicle airplane = new Airplane();
+        Vehicle train = new Train();
 
-        Animal[] animals = new Animal[]{cow, goose, dog, guineaPig};
-        for (Animal object : animals) {
-            object.sound();
+        Vehicle[] vehicles = new Vehicle[]{bus, airplane, train};
+        for (Vehicle object : vehicles) {
+            object.move();
         }
     }
 }
