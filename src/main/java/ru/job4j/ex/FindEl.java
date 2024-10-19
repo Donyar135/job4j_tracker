2b5830cfccbdb51923ed1451b3d0880cfd5575cb
Daniyar <aldanov2002@bk.ru>
2024-10-19 17:33:01 +0600

    0.4. Пользовательские исключения. [#219367 #518073]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,21 +1,22 @@
 package ru.job4j.ex;
 
+import java.util.Objects;
+
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
-        boolean h = true;
         for (int i = 0; i < value.length; i++) {
-            if (key == value[i]) {
-                h = false;
+            if (Objects.equals(value[i], key)) {
+                result = i;
             }
         }
-        if (h) {
+        if (result == -1) {
             throw new ElementNotFoundException("Element not found");
         }
         return result;
     }
 
-    public static void main(String[] args) throws ElementNotFoundException {
+    public static void main(String[] args) {
         try {
             indexOf(new String[] {"Артем", "Петр", "Данияр", "Александр"}, "Петр");
         } catch (ElementNotFoundException e) {
