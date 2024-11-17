ea2feaa9feea6a50bd15a2d26b25ec0f55a4271b
Daniyar <aldanov2002@bk.ru>
2024-11-17 13:49:34 +0600

    1. Интерфейс Queue [#504942 #521977]

--- src/main/java/ru/job4j/queue/AppleStore.java
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -14,19 +14,15 @@ public class AppleStore {
     }
 
     public String getLastHappyCustomer() {
-        for (int i = 0; i < queue.size(); i++) {
-            if (i != count - 1) {
-                queue.poll();
-            }
+        for (int i = 0; i < count - 1; i++) {
+            queue.poll();
         }
         return queue.peek().name();
     }
 
     public String getFirstUpsetCustomer() {
-        for (int i = 0; i < queue.size(); i++) {
-            if (i != count) {
-                queue.poll();
-            }
+        for (int i = 0; i < count; i++) {
+            queue.poll();
         }
         return queue.peek().name();
     }
