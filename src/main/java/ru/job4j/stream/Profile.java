5643e2a791d4d9235a929043d8e2edf656d50e82
Daniyar <aldanov2002@bk.ru>
2024-12-03 00:18:37 +0600

    7. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profile.java
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -2,12 +2,18 @@ package ru.job4j.stream;
 
 public class Profile {
     private Address address;
+    private String city;
 
-    public Profile(Address address) {
+    public Profile(Address address, String city) {
         this.address = address;
+        this.city = city;
     }
 
     public Address getAddress() {
         return address;
+    }
+
+    public String getCity() {
+        return city;
     }
 }
